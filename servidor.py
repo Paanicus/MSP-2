0e58a46af4a80e3c4706dec72784393c04cfed8d
Paanicus <EmanuelDOsechasL@gmail.com>
2024-05-22 22:29:01 +0000

    Mundo guardado 2024-05-22

--- servidor.py
+++ servidor.py
@@ -7,7 +7,6 @@
 # mohist / catserver   Install forge   first
 # purpur               Install fabric  first
 # snapshot             Install vanilla first
-
 # Ngrok region
 # Code           Place
 #-----------     ---------------------------
